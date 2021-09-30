4d5dc8289ae6dfcda8131b865028cb732ddb4c42
CLowans01 <64904892+CLowans01@users.noreply.github.com>
2021-09-30 12:37:40 +0100

    Update Script_v1.R

--- R/Script_v1.R
+++ R/Script_v1.R
@@ -1,9 +1,28 @@
+#C Lowans PhD work.
 # Script V1
+# citation for stplanr package:
+# Lovelace, R. and Ellison, R., 2017. stplanr: A Package for Transport Planning. The R Journal.
+# https://doi.org/10.32614/RJ-2018-053
 
 #Load packages
 library(stplanr)
 
+if (!require("pacman")) install.packages("pacman")
+
+#load any needed pacman packages
+# pacman::p_load(pacman, rio, ..)
 
 # load OD data
-
+# This bit not done with stplanr - need to clean and format etc
 #
+
+# create desire lines
+
+# is the data on mode available? if so, visualise by mode
+
+# load the API
+
+# Use API to route
+
+# Calc distances
+
