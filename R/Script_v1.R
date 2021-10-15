4909862dd3e577e2f644d7bc63330765108f91e9
Christopher Lowans <40055486@ads.qub.ac.uk>
2021-10-15 15:52:16 +0100

    Update Script_v1.R

--- R/Script_v1.R
+++ R/Script_v1.R
@@ -12,7 +12,9 @@ if (!require("pacman")) install.packages("pacman")
 #load any needed pacman packages
 # pacman::p_load(pacman, rio, ..)
 
-# load OD data
+# install.packages("tidyverse")
+
+# load OD data - also NB rename the data that you load - stplanr data has default names per intro vignette
 # This bit not done with stplanr - need to clean and format etc
 #
 
@@ -20,9 +22,25 @@ if (!require("pacman")) install.packages("pacman")
 
 # is the data on mode available? if so, visualise by mode
 
-# load the API
+# load the APIs
 
-# Use API to route
+# OSRM for cars
+
+# cycle streets.net for cycle lanes
+
+# Use APIs to route
 
 # Calc distances
 
+# compare euclidean distance with route distances & compare fastest vs quietest etc.for current travel patterns
+
+# catchment areas
+
+# reproduce input data for new scenarios with new modes assigned -> either scenarios or run modal split model elsewhere
+
+#rerunabove steps
+
+#visualise new networks & calculate travel times using google maps API or cycle streets API -> post hoc process to find ebike speed?
+
+# what are the new travel times of cars / buses with new levels of traffic?
+# Implicitly assume services are accessible in omagh town and new travel times increase accessibility?
